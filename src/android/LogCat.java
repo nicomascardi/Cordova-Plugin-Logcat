8bd173157331bb9ff7805c2c165bfbb7cf84e105
Nicolás Mascardi <nmascardi@440labs.com>
2017-03-09 10:41:29 -0300

    added logs

--- src/android/LogCat.java
+++ src/android/LogCat.java
@@ -17,7 +17,7 @@ public class LogCat extends CordovaPlugin {
 	    if (action.equals("sendLogs")) {
                         // save logcat in file
 				System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-				System.out.println(Environment.getExternalFilesDir());		
+				System.out.println(Environment.getExternalStoragePublicDirectory());		
                 File outputFile = new File(Environment.getExternalStorageDirectory(),
                         "logcat.txt");
                 try {
